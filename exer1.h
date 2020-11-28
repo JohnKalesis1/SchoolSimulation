8630eed6daf57c1f8e117fe639688197c40d018e
JohnKalesis1 <61728881+JohnKalesis1@users.noreply.github.com>
2020-11-28 14:58:39 +0200

    Update exer1.h

--- exer1.h
+++ exer1.h
@@ -6,23 +6,20 @@ class Teacher;
 
 class Classroom  {
     private:
-        std::string teacher;
+        std::string teacher;//since we only need the teacher's name, there is no need to have the entire object(only the Teacher stores the pointer to his clasroom)
         int capacity;
-        //int registered_students_count;
-        Student** students;
+        Student** students;//an array of pointers to Student objects
         int population;
         short id;
         bool entry_possible;
     public:
         void print();
-        void enter(Student* student);
+        void enter(Student* student);//stores the pointer of the student to the last position of the classrooms array
         bool teacher_inside();
         int get_capacity();
         short get_id();
-        //void assign_student(Student* student);
         void assign_teacher(std::string name);
         void teacher_enters();
-        //Classroom()  {}
         Classroom(int capacity,short id); 
         ~Classroom();
 };
@@ -33,15 +30,14 @@ class Corridor {
         int population;
         int capacity;
     public:
-        Student* forced_exit(short id);
+        Student* forced_exit(short id);//if a student of with the classroom id is waiting in the corridor, re-arrange his position  with the last of the array and call the exit function
         void print();
         bool is_empty();
         int get_student_class_id();
         int get_population();
         bool space_available();
-        void enter(Student* student);
-        Student* exit();
-        //Corridor()  {}
+        void enter(Student* student);//stores the pointer of the student to the last position of the array
+        Student* exit();//returns the pointer to the last student of the array and reduces the count of students
         Corridor(int capacity);
         ~Corridor();  
 };
@@ -55,12 +51,11 @@ class Stairs  {
     public:
         void print();
         bool is_empty();
-        short floor_distribution();
+        short floor_distribution();//returns the floor that the last student of the array must enter in order to reach his calssroom
         int get_population();
         bool space_available();
-        void enter(Student* student);
-        Student* exit();
-        //Stairs()  {}
+        void enter(Student* student);//same as previous 
+        Student* exit();//same as previous 
         Stairs(int capacity);
         ~Stairs(); 
 };
@@ -78,7 +73,6 @@ class Yard  {
         bool space_available();
         void enter(Student* student);
         Student* exit();
-        //Yard()  {}
         Yard(int capacity); 
         ~Yard(); 
 };
@@ -89,16 +83,13 @@ class Floor  {
         int s_count;
         Classroom* classrooms[6];
         Corridor* corridor;
-        Student** students;
         short height;
     public:
         Classroom* get_class(short number);
         void print();
-        bool space_available_in_corridor();
-        bool students_advance();
-        void enter(Student* student);
-        void flush();
-        //Floor()  {}
+        bool space_available_in_corridor();//returns true if there is space available in thefloor's corridor
+        void enter(Student* student);//enters the student into the corridor by calling the corridor's enter() function
+        bool flush();//force all the students in the corridor to try and enter their classrooms(explaining its significance in the function's body)
         Floor(int height,int corridor_capacity,int class_capacity);
         ~Floor();
 };
@@ -107,20 +98,18 @@ class SchoolBuilding  {
     private:
         Student** students;
         Teacher* teachers[18];
-        int advanced_count;
-        int s_count;
-        short t_count;
+        int advanced_count;//how many students have entered the school 
+        int s_count;//how many students are registered in the school
+        short t_count;//how many teachers are registered in the school
         Floor* floors[3];
         Yard* yard;
         Stairs* stairs;
+        void place_teacher(bool all);//a function that randomly picks a teacher and places him in his classroom(has a second mode where it places all the teachers when called)
     public:
-        void school_begins();
-        void stairs_exit();
-        bool exit(Student* student);
+        void school_begins();//initiate the advancement of students to reach their classrooms
         void register_student(std::string name);
         void register_teacher(std::string name);
         void print();
-        void place_teacher(bool all);
         SchoolBuilding(int class_capacity,int yard_capacity,int stairs_capacity,int corridor_capacity);
         ~SchoolBuilding();
 };
@@ -133,8 +122,7 @@ class Student  {
     public:
         short get_classroom_id();
         std::string get_name();
-        void set_location_to(short i);
-        //Student()  {}
+        void set_location_to(short i);//when a student advances to a new location, this will indicate where he is
         Student(std::string name,Classroom* classroom); 
         ~Student();
 };
@@ -145,7 +133,7 @@ class Teacher  {
         bool in_class;
         Classroom* classroom;
     public:
-        void enter_class();
+        void enter_class();//place the teacher in his classroom and prohibit all entry to it 
         bool is_in_class();
         std::string get_name() const;
         Teacher()  {  }
