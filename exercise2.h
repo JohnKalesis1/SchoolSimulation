c6479d0294ff47c5ea5dac78889d698251f4b298
JohnKalesis1 <61728881+JohnKalesis1@users.noreply.github.com>
2020-11-28 14:56:56 +0200

    Update exercise2.h

--- exercise2.h
+++ exercise2.h
@@ -6,35 +6,36 @@ class Student_Pair;
 
 class School  {
     private:
-        Sequence** arrangements;
-        Sequence* large_sequence;
+        Sequence** arrangements;//the arrangement of each classroom
+        Sequence* large_sequence;//the unified arrangemet, in which the lonely pairs of opposite gender have merged 
         int number_of_sequences;
-        void switch_member_of_pair(Student_Pair* pair1,Student_Pair* pair2,bool full_switch,bool gender);
-        void Set_availability_to_open();
-        Student_Pair* pick_random(Student_Pair* original_pair,int sequence_id,bool same_class,bool vvip_scenario,bool full_switch,bool gender_of_pair);
-        void swap(Student_Pair* pair,bool gender);
+        int number_of_students;
+        void switch_member_of_pair(Student_Pair* pair1,Student_Pair* pair2,bool full_switch,bool gender);//replaces both members if its a full_switch, or only the member of gender indicated otherewise
+        void Set_availability_to_open();//sets every sequences, open for the transfer of a consecutive naughty pair(the reason will become apparent laters when we use it)
+        Student_Pair* pick_random(Student_Pair* original_pair,int sequence_id,bool same_class,bool vvip_scenario,bool full_switch,bool gender_of_pair);//Pick a random pair from the sequence which satisfies the citeria listed 
+        void swap(Student_Pair* pair,bool gender);//causes a naughty pair or student to swap with another random, calling all necessary functions
     public:
         School(const std::string* boys,const std::string* girls,int size_boys,int size_girls,int number_of_classrooms,int mess_limit,int quiet_limit);
         ~School();
         void print_arrangements_per_classroom();  
         void print_unified_arrangement(); 
         void create_unified_sequence();
-        void behave(int L);
+        void behave(int L);//the algorithm where for L times, random students cause trouble and are replaced with their peers
 };
 
 class Student  {
     private: 
         std::string name;
-        Sequence* sequence;
-        bool mischievous;
+        Sequence* sequence;//We want to keep a pointer to the original sequence a student belonged(aka his classroom)
+        bool mischievous;//if the student is mischievous or not
         bool gender;
     public:
         Student(const std::string name,bool gender,Sequence* sequence);
         ~Student() {}
         Sequence* get_sequence();
         bool is_mischievous();
-        void causes_noise();
-        void repent();
+        void causes_noise();//apply the "mischievous" status effect to a student
+        void repent();//cure a student from the "mischievous status effect"
         bool get_gender();
         std::string get_name();
 };
@@ -43,12 +44,12 @@ class Student_Pair  {
     private:
         Student* Girl;
         Student* Boy;
-        Sequence* sequence;
-        short mode_of_tranfer;
-        bool vvip;
-        bool start_of_consecutive;
-        bool double_penalty;
-        short situation;
+        Sequence* sequence;//pairs are only the storage of students, so they do not switch and keeping their sequence is useful for the puropose of trying ot pick other pairs of different sequences
+        short mode_of_tranfer;//when swap is called on this pair, it will act based on the mode of transfer of the pair
+        bool vvip;//whether the pair is in a series of consecutive pairs more than 2, and thus needs special treatment to be placed in a different sequence than its other peers
+        bool start_of_consecutive;//whether or not the pair, is the first in a series of consecuitve pairs
+        bool double_penalty;//whether when swtching this pair, we need to penaltize its classroom by a double amount
+        short situation;//whether its a pair of only a girl, only a boy, or a complete pair
     public:
         Student_Pair() {}
         Student_Pair(const Student_Pair& pair);
@@ -63,7 +64,7 @@ class Student_Pair  {
         bool get_importance();
         void replace_member(Student* student,bool gender);
         short get_situation();
-        short bad_pair();
+        short bad_pair();//returns how bad is the pair
         int get_sequence_id();
         short get_mode_of_transfer();
         void set_mode_of_transfer(short mode);
@@ -71,7 +72,7 @@ class Student_Pair  {
         void award_vvip_status();
         void pay_and_repent(bool both_members,bool gender);
         bool cause_trouble(int& rolls,int max_rolls);
-        void complete_pair(Student_Pair* other_half,bool gender);
+        void complete_pair(Student_Pair* other_half,bool gender);//adds a second student to a lonely pair nad completes it
 };
 
 class Sequence  {
@@ -88,7 +89,7 @@ class Sequence  {
         Sequence(const std::string* boys,const std::string* girls,int min_size,int mode,int mess_limit,int quiet_limit,short id);
         ~Sequence();
         void add_to_arrangement(Student* Girl,Student* Boy);
-        void copy_pair(Student_Pair* pair);
+        void copy_pair(Student_Pair* pair);//we pass the pointer from another pair to the array of this sequence(will only occur when we create the unified seqeunce)
         void print();
         int get_id();
         int count_boys();
@@ -98,7 +99,7 @@ class Sequence  {
         bool get_availability();
         void set_availability_to(bool status);
         int get_pair_position();
-        void send_to_oblivion(Student_Pair* pair);
+        void send_to_oblivion(Student_Pair* pair);//destroys a pair without touching any of its members(will only occur on a lonely when a pair completion occurs, and thus the pair will no longer has any use)
         
         Student_Pair* get_nth_pair(int n);
 };
