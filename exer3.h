85b9815a229f422a412cf9c710441713ddb43420
JohnKalesis1 <61728881+JohnKalesis1@users.noreply.github.com>
2020-12-13 19:08:46 +0200

    Update exer3.h

--- exer3.h
+++ exer3.h
@@ -1,12 +1,12 @@
 #include <vector>
 #include <string>
-#include <iostream>
+
 
 
 class Classroom;
 
 class Person  {
-    private:
+    protected:
         std::string name;
         int exhaustion;
     public:
@@ -22,38 +22,41 @@ class Teacher: public Person {
         Classroom* classroom;
         bool inside_classroom;
     public: 
-        Teacher(Classroom* classroom,std::string name) : Person(name), inside_classroom(false)  {}
+        Teacher(Classroom* classroom,std::string name);
         ~Teacher() {}
         bool is_in_classroom();
         void teach(int hours);
-        void enters_classroom();
+        void place();
 
 
 };
 
 class Student: public Person {
-    private:
+    protected:
         Classroom* classroom;
         bool inside_classroom;
     public:
-        Student(Classroom* classroom,std::string name) : Person(name), inside_classroom(false) {}      
+        Student(Classroom* classroom,std::string name) : Person(name), inside_classroom(false), classroom(classroom) {}      
         virtual ~Student() {}
         virtual void attend(int hours)=0;
+        virtual void print()=0;
         void enters_classroom();
         int get_classroom_id();
 };
 
 class Junior : public Student {
     public:
-        Junior(Classroom* classroom,std::string name) : Student(classroom,name) {}
+        Junior(Classroom* classroom,std::string name);
         ~Junior() {}
+        void print();
         void attend(int hours);
 };
 
 class Senior : public Student {
     public:
-        Senior(Classroom* classroom,std::string name): Student(classroom,name) {}
+        Senior(Classroom* classroom,std::string name);
         ~Senior() {}
+        void print();
         void attend(int hours);
 };
 
@@ -62,14 +65,14 @@ class Space {
     protected:
         std::vector<Student*> students;
     public:
-        Space();
+        Space() {}
         ~Space() {}
-        virtual void enter(Student* student);
+        void enter(Student* student);
 };
 
 class Yard: public Space  {
     public:
-        Yard() {}
+        Yard();
         ~Yard() {}
         void enter(Student* student);
         Student* exit();
@@ -77,7 +80,7 @@ class Yard: public Space  {
 
 class Stairs: public Space  {
     public:
-        Stairs() {}
+        Stairs();
         ~Stairs() {}
         void enter(Student* student);
         Student* exit();
@@ -86,7 +89,7 @@ class Stairs: public Space  {
 
 class Corridor: public Space  {
     public:
-        Corridor() {}
+        Corridor();
         ~Corridor() {}
         void enter(Student* student);
         Student* exit();
@@ -97,8 +100,8 @@ class Classroom : public Space {
         int id;
         Teacher* teacher;
     public:
-        Classroom(int id) : id(id)  {std::cout << "Classroom created" << id << std::endl;}
-        ~Classroom();
+        Classroom(int id);
+        ~Classroom() {}
         void register_teacher(Teacher* teacher);
         int get_id();
         void print();
@@ -112,7 +115,9 @@ class Floor  {
         std::vector<Classroom> classrooms;
         int floor_number;
     public:
-        Floor(int floor_number,int class_capacity);
+        Floor(int floor_number);
+        ~Floor() {}
+        Classroom* get_classroom(int index);
         void enter(Student* student);
         void operate(int hours);
         void print();
@@ -121,22 +126,25 @@ class Floor  {
 class School {
     private:
         std::vector<Student*> students;
-        std::vector<Teacher> teachers;
+        std::vector<Teacher*> teachers;
         std::vector<Floor> floors;
         Yard yard;
         Stairs stairs;
+        int juniors_iterator;
+        int seniors_iterator;
         static inline int Junior_exhaustion_rate=0;
         static inline int Senior_exhaustion_rate=0;
         static inline int Teacher_exhaustion_rate=0;
     public:
         School(int Lj,int Ls,int Lt); 
         ~School();
-        void register_student();
-        void register_teacher();
+        void register_senior(std::string name);
+        void register_junior(std::string name);
+        void register_teacher(std::string name);
         static int get_junior_exhaustion_rate();
         static int get_senior_exhaustion_rate();
         static int get_teacher_exhaustion_rate();
-        void school_begins();
+        void begins();
         void print();
         void operate(int hours);
-};  
+};
